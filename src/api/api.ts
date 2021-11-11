7fe8270d4ac9714fa268fe46fa73794b700fff94
Kaio <kaio_matos22@hotmail.com>
2021-11-11 09:54:33 -0300

    Adding API class

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,1 +1,14 @@
-export {};
+class Meal {
+  baseURL = "https://www.themealdb.com/api/json/v1/1";
+
+  async getRamdom() {
+    const raw = await fetch(`${this.baseURL}/random`);
+    const mealData: any = await raw.json();
+
+    console.log(mealData);
+    return mealData;
+  }
+}
+
+const MealAPI = new Meal();
+export { MealAPI };
